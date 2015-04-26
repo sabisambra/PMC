4b64eb91af4e717773e97ad1adf0550d4351ad51
sabisambra <s.abisambra125@uniandes.edu.co>
2015-04-26 18:54:32 -0500

    Profile

--- MyApplication/app/src/main/java/com/example/santiago/myapplication/Profile.java
+++ MyApplication/app/src/main/java/com/example/santiago/myapplication/Profile.java
@@ -1,26 +1,24 @@
 package com.example.santiago.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class SignIn extends ActionBarActivity {
+public class Profile extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_profile);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sign_in, menu);
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
         return true;
     }
 
@@ -37,13 +35,5 @@ public class SignIn extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void entrar(View view)
-    {
-        Intent intent = new Intent(this,Profile.class);
-        Bundle bundle = new Bundle();
-        intent.putExtras(bundle);
-        startActivity(intent);
     }
 }
